dc004b303e7b41f75c04722faa55bce3e5fe9be1
deashadow <67285924+deashadow@users.noreply.github.com>
2021-01-15 19:14:01 -0500

    updated dbquery codes added additional file called addQuestion.js added additional routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./Home";
 import Forum from "./Forum";
 import ContactUs from "./ContactUs";
 import AboutUs from "./AboutUs";
+import addQuestion from "./addQuestion";
 import NavB from "./NavB";
 import { BrowserRouter, Route, Switch, Prompt } from "react-router-dom";
 import  Login from "./Login";
@@ -23,6 +24,7 @@ function App() {
           <Route path="/Forum"  component={Forum} />
           <Route path="/AboutUs" component={AboutUs} />
           <Route path="/ContactUs" component={ContactUs} />
+          <Route path="/addQuestion" component={addQuestion} />
           <Prompt message="Are you sure you want to leave??"/>
         </Switch>
       </div>
